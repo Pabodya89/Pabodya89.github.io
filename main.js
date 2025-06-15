008f8405ea820ee5b538edc3bcdcd28b3836fb69
Pabodya89 <2022cs209@stu.ucsc.cmb.ac.lk>
2025-06-15 12:47:46 +0530

    "potpoliyo codes"

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".text", {
+        strings: ["Full Stack Developer", "Youtuber", "Data Science Engineer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
